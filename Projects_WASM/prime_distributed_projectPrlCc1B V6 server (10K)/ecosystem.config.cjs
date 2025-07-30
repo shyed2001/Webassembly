82204ba1dbc4a5e33ed11c5a9f72a3cdd800fdcb
Shyed Shahriar <shyed2001@yahoo.com>
2025-07-30 19:02:48 +0600

    ok
    ok

---
+++ Projects_WASM/prime_distributed_projectPrlCc1B V6 server (10K)/ecosystem.config.cjs
@@ -0,0 +1,21 @@
+// In: prime_distributed_projectPrlCc1B V5(10K)/coordinator_server.js
+module.exports = {
+  apps : [{
+    name   : "coordinator",
+    script : "coordinator_server.js",
+    // ⬇️ *** CHANGE THIS LINE TO FALSE *** ⬇️
+    watch  : false,
+    env    : {
+      NODE_ENV: "development",
+    }
+  }, {
+    name   : "http-server",
+    script : "python3",
+    args   : "-m http.server 8008 --bind 0.0.0.0",
+    cwd    : "./public",
+    watch  : false,
+    env    : {
+      NODE_ENV: "development",
+    }
+  }]
+}
